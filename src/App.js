c66a9d779874ed9acff3c44527d5576128b6aba8
Chris Gaona <thebeloved88@sbcglobal.net>
2017-11-11 14:25:46 -0800

    Adding Guests to the list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
 
   state = {
     isFiltered: false,
+    pendingGuest: "",
     guests: [
       {
         name: "Treasure",
@@ -67,6 +68,25 @@ class App extends Component {
   toggleFilter = () =>
     this.setState({isFiltered: !this.state.isFiltered});
 
+  handleNameInput = e =>
+    this.setState({pendingGuest: e.target.value});
+
+  newGuestSubmitHandler = e => {
+    e.preventDefault();
+    this.setState({
+      guests: [
+        {
+          name: this.state.pendingGuest,
+          isConfirmed: false,
+          isEditing: false
+        },
+        // use spread operator to fill new guests object with other key/values
+        ...this.state.guests
+      ],
+      pendingGuest: "",
+    });
+  };
+
   getTotalInvited = () => this.state.guests.length;
 
   // getAttendingGuests = () =>
@@ -78,8 +98,12 @@ class App extends Component {
         <header>
           <h1>RSVP</h1>
           <p>A Treehouse App</p>
-          <form>
-            <input type="text" value="Safia" placeholder="Invite Someone" />
+          <form onSubmit={this.newGuestSubmitHandler}>
+            <input
+              type="text"
+              onChange={this.handleNameInput}
+              value={this.state.pendingGuest}
+              placeholder="Invite Someone" />
               <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
